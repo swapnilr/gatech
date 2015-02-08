94eb881a0d371e0eb8aba8fa96913d02ebf490f8
Swapnil Ralhan <swapnilr@swapnilr-macbookpro.roam.corp.google.com>
2015-02-07 17:14:06 -0800

    Adding partially finished hough lines draw, need some normalizing

--- omscs/vision-4495/ps1/hough_lines_draw.py
+++ omscs/vision-4495/ps1/hough_lines_draw.py
@@ -1,11 +1,28 @@
+import cv2
+import numpy as np
+import cv
+
 def hough_lines_draw(img, outfile, peaks, rho, theta):
-#     % Draw lines found in an image using Hough transform.
-#     %
-#     % img: Image on top of which to draw lines
-#     % outfile: Output image filename to save plot as
-#     % peaks: Qx2 matrix containing row, column indices of the Q peaks found in accumulator
-#     % rho: Vector of rho values, in pixels
-#     % theta: Vector of theta values, in degrees
-# 
-#     % TODO: Your code here
-    img = 0
+    #     % Draw lines found in an image using Hough transform.
+    #     %
+    #     % img: Image on top of which to draw lines
+    #     % outfile: Output image filename to save plot as
+    #     % peaks: Qx2 matrix containing row, column indices of the Q peaks found in accumulator
+    #     % rho: Vector of rho values, in pixels
+    #     % theta: Vector of theta values, in degrees
+    # 
+    #     % TODO: Your code here
+    rgbImage = cv2.cvtColor(img, cv.CV_GRAY2RGB)
+    for col in range(img.shape[1]):
+        for peak in peaks:
+            t = np.radians(theta[peak[1]])
+            p = rho[peak[0]]
+            if t == 0:
+                for row in range(img.shape[0]):
+                    rgbImage[row, col, 2] = 255
+            else:
+                print peak, peak[1], theta[peak[1]], t, np.tan(t), np.sin(t)
+                row = -(1/np.tan(t)) * col + (p * (1/np.sin(t)))
+                rgbImage[row,col,2] = 255
+    cv2.imshow(rgbImage)
+    return rgbImage
--- omscs/vision-4495/ps1/test1.py
+++ omscs/vision-4495/ps1/test1.py
@@ -1,12 +1,15 @@
 import cv2
+from hough_lines_acc import hough_lines_acc
+from hough_peaks import hough_peaks
+import time
+from hough_lines_draw import hough_lines_draw
+
 i1 = cv2.imread('input/ps1-input0.png',0)
 edges = cv2.Canny(i1, 100, 200)
-from hough_lines_acc import hough_lines_acc
 H, t, r = hough_lines_acc(edges)
 cv2.imshow("H", H)
-from hough_peaks import hough_peaks
 peaks = hough_peaks(H, 10)
 print peaks
-import time
+hough_lines_draw(i1, None, peaks, r, t)
 time.sleep(10)
 cv2.destroyAllWindows()
