01a7a45a8fd5d3ab585b19c81d8106e851bb4322
Swapnil Ralhan <swapnilr@swapnilr-macbookpro.roam.corp.google.com>
2015-02-22 17:26:06 -0800

    Implemented both the functions

--- omscs/vision-4495/ps2/disparity_ncorr.py
+++ omscs/vision-4495/ps2/disparity_ncorr.py
@@ -1,3 +1,6 @@
+import numpy as np
+import cv2
+
 def disparity_ncorr(L, R):
     """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
     
@@ -7,5 +10,23 @@ def disparity_ncorr(L, R):
 
     Returns: Disparity map, same size as L, R
     """
-
-    # TODO: Your code here
+    rows, columns = L.shape
+    DSI = np.zeros((rows, columns, columns))
+    boxsize = 5
+    im_filter = np.ones((boxsize, boxsize)) #/ (boxsize * boxsize)
+    #s = np.sqrt((L*L) * (R*R))
+    for d in range(0, columns):
+        rshift = np.roll(R, -d, axis = 1)
+        Rtx = cv2.filter2D(L * rshift, ddepth=-1, kernel=im_filter)
+        Rxx = cv2.filter2D(L * L, ddepth=-1, kernel=im_filter)
+        Rtt = cv2.filter2D(rshift * rshift, ddepth=-1, kernel=im_filter)
+        mult = Rtx / np.sqrt(Rxx * Rtt) 
+        #ncorr = cv2.filter2D(mult, ddepth=-1, kernel=im_filter)
+        DSI[:,:,d] = mult #ncorr #cv2.filter2D(Rxx * Rtt, ddepth=-1, kernel=im_filter))
+    D = np.argmax(DSI, axis=2)
+    for y in range(rows):
+        for x in range(columns):
+            if D[y,x] + x > columns:
+                D[y,x] = D[y,x] - columns
+    print D.shape
+    return D
--- omscs/vision-4495/ps2/disparity_ssd.py
+++ omscs/vision-4495/ps2/disparity_ssd.py
@@ -1,3 +1,6 @@
+import numpy as np
+import cv2
+
 def disparity_ssd(L, R):
     """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
     
@@ -7,5 +10,20 @@ def disparity_ssd(L, R):
 
     Returns: Disparity map, same size as L, R
     """
-
-    # TODO: Your code here
+    rows, columns = L.shape
+    DSI = np.zeros((rows, columns, columns))
+    boxsize = 5
+    im_filter = np.ones((boxsize, boxsize)) / (boxsize * boxsize)
+    for d in range(0, columns):
+        rshift = np.roll(R, -d , axis = 1)
+        #rshift = np.pad(R, ((0,0), (0,d)), mode='symmetric')[:,d:]
+        diff = np.subtract(L, rshift)
+        diff = np.power(diff, 2)
+        ssd = cv2.filter2D(diff, ddepth=-1, kernel=im_filter)
+        DSI[:,:,d] = ssd
+    D = np.argmin(DSI, axis=2)
+    for y in range(rows):
+        for x in range(columns):
+            if D[y,x] + x > columns:
+                D[y,x] = D[y,x] - columns
+    return D
