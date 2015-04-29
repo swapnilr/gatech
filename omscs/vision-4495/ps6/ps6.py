98e05b2e03072e3af252ff755006c8d26cd7b65b
Swapnil Ralhan <swapnilr@swapnilr-macbookpro.roam.corp.google.com>
2015-04-28 19:19:19 -0700

    PS6 updated

--- omscs/vision-4495/ps6/ps6.py
+++ omscs/vision-4495/ps6/ps6.py
@@ -2,13 +2,19 @@ import cv2
 import numpy as np
 from collections import namedtuple
 import math
+import functools
+import time
 
 FILES = ['pres_debate.avi', 'noisy_debate.avi', 'pedestrians.avi']
 
 Model = namedtuple('Model', ['x', 'y', 'w', 'h'], verbose=True)
 
-NUM_PARTICLES = 196
+NUM_PARTICLES = 1000
 SIGMA_MSE = 10
+ALPHA = 0.95
+
+def getHandModel():
+    return Model(x=520, y=385, w=90, h=130)
 
 def getModel(index=0):
     return createModel(getTextFile(getFilename(index)))
@@ -29,11 +35,18 @@ def getFilename(index=0):
 def upscale(image):
     return (image * 255).astype(np.uint8)
 
+def downscale(image):
+    return image.astype(np.float) / 255
+
 class Filter():
 
     def __init__(self, image, template):
         self.image = image
         self.template = template
+
+    # Model should take in 2 templates, the current template and the best guess
+    def setAppearanceModel(self, model):
+        self.appearance_model = model
 
     # Model is a function that takes in an (y,x) tuple and returns another
     # (y,x) tuple.
@@ -63,70 +76,139 @@ class Filter():
             for j in range(val):
                 final_particles[loc] = new_particles[index]
                 loc += 1
-        return final_particles
 
-def gaussNoiseDynamicsModel(particle):
-    return np.around(np.random.normal(size=2) + particle)
+        height, width = self.template.shape
+        pindex = np.argmax(probs)
+        particle = new_particles[pindex]
+        v = particle[0] - height/2
+        u = particle[1] - width/2
+        patch = self.image[v:v+height,u:u+width]
+        self.template = self.appearance_model(self.template, patch)
+        return final_particles, particle, self.template
 
-def MSESensorModel(image, template, particle):
+def simpleAppearanceModel(template, best, alpha=ALPHA):
+    return template
+
+def alphaAppearanceModel(template, best, alpha=ALPHA):
+    return alpha * best + (1 - alpha) * template
+
+def gaussNoiseDynamicsModel(particle, sigma=1.0):
+    return np.around(np.random.normal(scale=sigma,size=2) + particle)
+
+
+def MSESensorModel(image, template, particle, sigma_mse=SIGMA_MSE):
     height, width = template.shape
     MSE = 0.
     #print particle
-    v = particle[0] - height/2
-    u = particle[1] - width/2
-    #print particle, height, width, v, u 
+    v = max(0, particle[0] - height/2)
+    u = max(0, particle[1] - width/2)
+    if v + height > image.shape[0]:
+        v = image.shape[0] - (v + height)
+    if u + width > image.shape[1]:
+        u = image.shape[1] - (u + width)
+    #print particle, height, width, v, u
+
     patch = image[v:v+height,u:u+width]
     #print template.shape, patch.shape
-    MSE = ((template - patch)**2).mean()
+    th, tw = patch.shape
+    if th != height or tw != width:
+        print "Size is wrong"
+        print particle, height, width, v, u
+        patch = np.zeros((template.shape))
+    MSE = np.mean(((template - patch)**2))
     #for y in range(height):
     #    for x in range(width):
     #        #print template[y,x], image[y+v, x+u]
     #        MSE += (template[y,x] - image[y+v, x+u])**2
     #MSE = MSE/(height*width)
     #print MSE
-    return math.exp(-MSE/(2 * (SIGMA_MSE ** 2)))
+    return math.exp(-MSE/(2 * (sigma_mse ** 2)))
 
 def q1():
-    debate = cv2.VideoCapture(getFilename(0))
+    # Part A
+    test(simpleAppearanceModel, stopPoints=[28,84,144], savePatchFile=True, sigma_mse=2)
+    # Part B
+    test(simpleAppearanceModel, stopPoints=[14,32,46], part='e', imgNum=1)
+
+def q2():
+    # Part A
+    test(alphaAppearanceModel, sigma=4.1, alpha=0.9, 
+         dynamicsModel=functools.partial(gaussNoiseDynamicsModel, sigma=4.1),
+         stopPoints=[15,50,140], model=getHandModel(), savePatchFile=True,
+         qn=2)
+    # Part B
+    test(alphaAppearanceModel, sigma=4.4, alpha=0.95, 
+         dynamicsModel=functools.partial(gaussNoiseDynamicsModel, sigma=4.4),
+         stopPoints=[15,50,140], model=getHandModel(), savePatchFile=True,
+         qn=2, part='b', imgNum=1)
+
+def test(appearanceModel, sigma=0.0,
+         dynamicsModel=gaussNoiseDynamicsModel, stopPoints=None, 
+         model=None, alpha=ALPHA, savePatchFile=False, qn=1,part='a', 
+         imgNum=0, sigma_mse=SIGMA_MSE):
+    stopPoints = stopPoints or []
+    debate = cv2.VideoCapture(getFilename(imgNum))
     index = 1
     ret, frame = debate.read()
     frame = (cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)).astype(np.float)
-    model = getModel(0)
+    model = model or getModel(imgNum)
     template = frame[int(math.floor(model.y)):int(math.ceil(model.y+model.h)), int(math.floor(model.x)):int(math.ceil(model.x+model.w)) ]
     index += 1
+
+    count = 1
+    if savePatchFile:
+        cv2.imwrite("output/ps6-%d-%s-%d.png" % (qn, part, count), upscale(template))
+        count += 1
+    #import time
+    #time.sleep(15)
 
     particles = []
     size = int(math.ceil(math.sqrt(NUM_PARTICLES)))
     rows, columns = frame.shape
-    ys = np.linspace(int(model.h/2) + 1, rows-int(model.h/2) - 1, size, endpoint=False)
-    xs = np.linspace(int(model.w/2) + 1, columns-int(model.w/2) - 1, size, endpoint=False)
+    ys = np.around(np.linspace(int(model.h/2) + 1, rows-int(model.h/2) - 1, size, endpoint=False))
+    xs = np.around(np.linspace(int(model.w/2) + 1, columns-int(model.w/2) - 1, size, endpoint=False))
+    #ys = np.around(np.linspace(int(model.y), int(model.y+model.h), size, endpoint=False))
+    #xs = np.around(np.linspace(int(model.x), int(model.x+model.w), size, endpoint=False))
+    #print ys
+    #print xs
     for y in ys:
         for x in xs:
             particles.append(np.array((y,x)))
 
     while debate.isOpened():
         ret, frame = debate.read()
+        orig = frame
         if not ret:
             print "ret is False at index %d" % index
             break
         frame = (cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)).astype(np.float)
         f = Filter(frame, template)
-        f.setDynamicsModel(gaussNoiseDynamicsModel)
-        f.setSensorModel(MSESensorModel)
-        particles = f.compute(particles)
-        if index == 28 or index == 84 or index == 144:
+        f.setAppearanceModel(functools.partial(appearanceModel, alpha=alpha))
+        f.setDynamicsModel(dynamicsModel)
+        f.setSensorModel(functools.partial(MSESensorModel, sigma_mse=sigma_mse))
+        particles, best, template = f.compute(particles)
+        if index in stopPoints:
             frame = cv2.cvtColor(upscale(frame), cv2.COLOR_GRAY2RGB)
             for particle in particles:
                 pt = (int(particle[1]), int(particle[0]))
-                cv2.circle(frame, pt, 1, (0, 0, 255))
-            cv2.imshow("%d" % index, frame)
-            import time
+                cv2.circle(orig, pt, 1, (0, 0, 255))
+            pt = (int(best[1]), int(best[0]))
+            cv2.circle(orig, pt, 5, (255, 0, 0))
+            height, width = template.shape
+            top = (pt[0] - width/2, pt[1] - height/2)
+            bottom = (pt[0]+width/2, pt[1] + height/2)
+            cv2.rectangle(orig, top, bottom, (255, 0, 0))
+            cv2.imwrite("output/ps6-%d-%s-%d.png" % (qn, part, count), orig)
+            count += 1
             time.sleep(5)
         index += 1
+        if index > 150:
+            break
         #print index
 
 def main():
     q1()
+    q2()
 
 if __name__ == '__main__':
     main()
